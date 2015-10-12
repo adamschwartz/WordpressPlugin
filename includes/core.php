55751cc85c47c8754b93f974f0365868d93639b6
Zack Bloom <zackbloom@gmail.com>
2015-10-11 22:01:39 -0400

    Fix bug in loading code

--- includes/core.php
+++ includes/core.php
@@ -1,9 +1,6 @@
 <?php
 function eager_get_embed_html() {
-  $code = eager_get_embed_code();
-  if ($code){
-    echo '<script data-cfasync="false" src="//fast.eager.io/{{.EmbedCode}}.js"></script>';
-  }
+  echo '<script data-cfasync="false" src="//fast.eager.io/{{.EmbedCode}}.js"></script>';
 }
 
 add_action('wp_head', 'eager_get_embed_html');
